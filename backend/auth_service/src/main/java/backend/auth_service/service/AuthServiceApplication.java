1c56399ee1b56a23a00c8089114020b7a630dc46
Cherry Li <licheukyan12@gmail.com>
2024-10-14 15:39:59 -0700

    fix AuthServiceApplication

--- backend/auth_service/src/main/java/backend/auth_service/service/AuthServiceApplication.java
+++ backend/auth_service/src/main/java/backend/auth_service/service/AuthServiceApplication.java
@@ -1,4 +1,4 @@
-package backend.auth_service.service;
+package backend.auth_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
