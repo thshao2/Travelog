7bf79956fed081f0d318d2b8ebd454b738bc5380
Brian Nguyen <nguyenbriankhoi@gmail.com>
2024-10-13 22:58:27 -0700

    created base dirs for media, travel, user microservice but NO docker modification yet

---
+++ backend/media_service/src/test/java/backend/media_service/MediaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.media_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
