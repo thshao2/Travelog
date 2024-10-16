ed586878f95a085ba2b5bc096c05c7051b3d8c04
Brian Nguyen <nguyenbriankhoi@gmail.com>
2024-10-15 21:44:09 -0700

    updated date fields in pin entity

--- backend/travel_service/src/main/java/backend/travel_service/entity/Pin.java
+++ backend/travel_service/src/main/java/backend/travel_service/entity/Pin.java
@@ -2,17 +2,12 @@ package backend.travel_service.entity;
 
 import java.time.LocalDateTime;
 
-import backend.travel_service.entity.Memory;
-import backend.travel_service.entity.Location;
-
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.PrePersist;
-import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -37,20 +32,12 @@ public class Pin {
     @Column(nullable = false)
     private Long userId; // FK to User in User Service
 
-    @Column(nullable = false, updatable = false)
-    private LocalDateTime createdAt;
-
     @Column(nullable = false)
-    private LocalDateTime updatedAt;
+    private LocalDateTime createdAt;
 
     @PrePersist
     protected void onCreate() {
         this.createdAt = LocalDateTime.now();
-        this.updatedAt = LocalDateTime.now();
     }
 
-    @PreUpdate
-    protected void onUpdate() {
-        this.updatedAt = LocalDateTime.now();
-    }
 }
