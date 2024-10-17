8d0d58eaf899a04ea05ba93ca97394cd749ad366
thshao <thshao@ucsc.edu>
2024-10-16 22:37:32 -0700

    setup interactive map with Mapbox

---
+++ TravelogUI2/app/(tabs)/something.tsx
@@ -0,0 +1,72 @@
+import { useRef, useEffect, useState } from 'react';
+import mapboxgl from 'mapbox-gl';
+
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+import './App.css';
+
+const INITIAL_CENTER = [
+  -122.0626,
+  37.0003
+]
+const INITIAL_ZOOM = 18.12
+
+function Map() {
+
+  const mapRef = useRef();
+  const mapContainerRef = useRef();
+
+  const [center, setCenter] = useState(INITIAL_CENTER)
+  const [zoom, setZoom] = useState(INITIAL_ZOOM)
+
+  useEffect(() => {
+    mapboxgl.accessToken = 'pk.eyJ1IjoidGhzaGFvIiwiYSI6ImNtMmN0cDV4dzE1ZXcybHE0aHZncWkybzYifQ.fRl3Y5un5jRiop-3EZrJCg'
+    mapRef.current = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      center: center,
+      zoom: zoom,
+    });
+
+    mapRef.current.on('move', () => {
+      // get the current center coordinates and zoom level from the map
+      const mapCenter = mapRef.current.getCenter()
+      const mapZoom = mapRef.current.getZoom()
+
+      // update state
+      setCenter([mapCenter.lng, mapCenter.lat])
+      setZoom(mapZoom)
+    })
+
+    return () => {
+      mapRef.current.remove();
+    }
+  }, [])
+
+  const handleButtonClick = () => {
+    mapRef.current.flyTo({
+      center: INITIAL_CENTER,
+      zoom: INITIAL_ZOOM
+    })
+  }
+
+
+  return (
+    <>
+      <div className="sidebar">
+        Longitude: {center[0].toFixed(4)} | Latitude: {center[1].toFixed(4)} | Zoom: {zoom.toFixed(2)}
+      </div>
+      <button className='reset-button' onClick={handleButtonClick}>
+        Reset
+      </button>
+      <div id='map-container' ref={mapContainerRef} />
+    </>
+  )
+}
+
+
+
+export default function something() {
+  return (
+    <Map />
+  )
+}
