9ba10cbf61f2c4f2c902c44a3a92c56d5a49e4af
thshao <thshao@ucsc.edu>
2024-10-18 09:36:38 -0700

    fix typescript errors in map.tsx

--- TravelogUI2/app/(tabs)/map.tsx
+++ TravelogUI2/app/(tabs)/map.tsx
@@ -1,11 +1,11 @@
 import { useRef, useEffect, useState } from 'react';
-import mapboxgl from 'mapbox-gl';
+import mapboxgl, { Map as MapboxMap } from 'mapbox-gl';
 
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 import './Map.css';
 
-const INITIAL_CENTER = [
+const INITIAL_CENTER: [number, number] = [
   -122.0626,
   37.0003
 ]
@@ -13,24 +13,27 @@ const INITIAL_ZOOM = 18.12
 
 function Map() {
 
-  const mapRef = useRef();
-  const mapContainerRef = useRef();
+  const mapRef = useRef<MapboxMap | null>(null);
+  const mapContainerRef = useRef<HTMLDivElement | null>(null); // type for HTML div element
 
-  const [center, setCenter] = useState(INITIAL_CENTER)
-  const [zoom, setZoom] = useState(INITIAL_ZOOM)
+  const [center, setCenter] = useState<[number, number]>(INITIAL_CENTER)
+  const [zoom, setZoom] = useState<number>(INITIAL_ZOOM)
 
   useEffect(() => {
     mapboxgl.accessToken = 'pk.eyJ1IjoidGhzaGFvIiwiYSI6ImNtMmN0cDV4dzE1ZXcybHE0aHZncWkybzYifQ.fRl3Y5un5jRiop-3EZrJCg'
     mapRef.current = new mapboxgl.Map({
-      container: mapContainerRef.current,
+      container: mapContainerRef.current ? mapContainerRef.current : '',
       center: center,
       zoom: zoom,
     });
 
     mapRef.current.on('move', () => {
       // get the current center coordinates and zoom level from the map
-      const mapCenter = mapRef.current.getCenter()
-      const mapZoom = mapRef.current.getZoom()
+      const mapCenter = mapRef.current ? mapRef.current.getCenter() : {lng: -122.06258247708297, lat: 37.0003006998805}
+      const mapZoom = mapRef.current ? mapRef.current.getZoom() : 18.12
+      // console.log(mapRef.current?.getCenter());
+      // console.log(mapRef.current?.getZoom());
+
 
       // update state
       setCenter([mapCenter.lng, mapCenter.lat])
@@ -38,12 +41,12 @@ function Map() {
     })
 
     return () => {
-      mapRef.current.remove();
+      mapRef.current?.remove();
     }
   }, [])
 
   const handleButtonClick = () => {
-    mapRef.current.flyTo({
+    mapRef.current?.flyTo({
       center: INITIAL_CENTER,
       zoom: INITIAL_ZOOM
     })
