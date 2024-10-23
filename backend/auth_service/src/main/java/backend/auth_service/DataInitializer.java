07988cc4e0a2fd22b01095c5e36625f509ba21a0
thshao <thshao@ucsc.edu>
2024-10-23 00:37:39 -0700

    add more user entries to db

--- backend/auth_service/src/main/java/backend/auth_service/DataInitializer.java
+++ backend/auth_service/src/main/java/backend/auth_service/DataInitializer.java
@@ -19,9 +19,14 @@ public class DataInitializer implements CommandLineRunner {
     public void run(String... args) throws Exception {
         // Encode the password
         String hashedPassword = passwordEncoder.encode("travelog");
+        String hashedPassword2 = passwordEncoder.encode("mollymember");
+        String hashedPassword3 = passwordEncoder.encode("annaadmin");
 
         // Insert the dummy user with hashed password
         jdbcTemplate.update("INSERT INTO users (username, password, email) VALUES (?, ?, ?)", "travelog", hashedPassword, "travelog@gmail.com");
+        jdbcTemplate.update("INSERT INTO users (username, password, email) VALUES (?, ?, ?)", "Molly Member", hashedPassword2, "molly@gmail.com");
+        jdbcTemplate.update("INSERT INTO users (username, password, email) VALUES (?, ?, ?)", "Anna Admin", hashedPassword3, "anna@gmail.com");
+
 
         System.out.println("Dummy data initialized with hashed password.");
     }
