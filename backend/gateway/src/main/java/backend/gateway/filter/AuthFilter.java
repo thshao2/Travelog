937182ef23d962a6d230d9f792d890f42a12ab3d
Emily Ho <icianho@gmail.com>
2024-10-27 01:58:54 -0700

    Added User ID header to request instead of response

--- backend/gateway/src/main/java/backend/gateway/filter/AuthFilter.java
+++ backend/gateway/src/main/java/backend/gateway/filter/AuthFilter.java
@@ -134,24 +134,15 @@ public class AuthFilter implements GlobalFilter, Ordered {
 
         // Attach UserId to header (if relavant)
         // If userId is present, attach it to the request header
-        // if (userId != null) {
-        //     exchange.getResponse().getHeaders().add("X-User-Id", String.valueOf(userId));
-        //     System.out.println("X-User-Id attached to request: " + userId);
-        // } else {
-        //     return handleError(exchange, "Unauthorized - token validation failed");
-        // }
-
-        if (userId != null) {
-            ServerHttpRequest mutatedRequest = request.mutate()
-                    .header("X-User-Id", String.valueOf(userId))
-                    .build();
-            exchange = exchange.mutate().request(mutatedRequest).build();
-            System.out.println("X-User-Id attached to request: " + userId);
-        } else {
+        if (userId == null) {
             return handleError(exchange, "Unauthorized - token validation failed");
-        }
-
-        return chain.filter(exchange);
+        } 
+        ServerHttpRequest updatedRequest = exchange.getRequest()
+                .mutate()
+                .header("X-User-Id", Long.toString(userId))
+                .build();
+        ServerWebExchange mutatedExchange = exchange.mutate().request(updatedRequest).build();
+        return chain.filter(mutatedExchange);
     }
 
     /**
