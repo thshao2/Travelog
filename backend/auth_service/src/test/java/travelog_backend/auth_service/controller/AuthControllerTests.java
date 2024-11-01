b1cc6604c8ee54e76a01118296d16e0335e2e7f5
fyyshih <felithecity47@gmail.com>
2024-11-01 04:50:55 -0700

    auth, travel, user controller testing infra

--- backend/auth_service/src/test/java/travelog_backend/auth_service/controller/AuthControllerTests.java
+++ backend/auth_service/src/test/java/travelog_backend/auth_service/controller/AuthControllerTests.java
@@ -6,25 +6,49 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
-import backend.auth_service.controller.AuthController;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+import backend.auth_service.controller.AuthController;
+import backend.auth_service.repository.UserRepository;
+import backend.auth_service.service.AuthService;
+import backend.auth_service.service.JwtService;
+import backend.auth_service.TestApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@WebMvcTest(AuthController.class)
+@AutoConfigureMockMvc(addFilters = false) // disable security for testing
+@ContextConfiguration(classes = TestApplication.class, locations = "backend.auth_service") // need spring context config for testing 
 public class AuthControllerTests {
 
 	@Autowired
-	private MockMvc mockMvc;
+    private MockMvc mockMvc;
 
-	@Test
-	void shouldReturnDefaultMessage() throws Exception {
-		this.mockMvc.perform(get("/auth/signup")).andDo(print()).andExpect(status().isOk())
-				.andExpect(content().string(containsString("Hello, World")));
-	}
+	@Mock
+	private AuthService authService;
+
+	@Mock
+	private JwtService jwtService;
+
+	@Mock
+    private UserRepository userRepository;
+
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    public void setUp() {
+        objectMapper = new ObjectMapper(); // // json format for making api requests
+    }
+
+	// @Test
 }
