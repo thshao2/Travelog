1c931978a09bac260c7a04a7cdc3808b91f3ee23
Brian Nguyen <nguyenbriankhoi@gmail.com>
2024-11-06 23:56:41 -0800

    added new linting rules

--- TravelogUI2/.eslintrc.js
+++ TravelogUI2/.eslintrc.js
@@ -1,11 +1,15 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-  extends: 'expo',
+  extends: "expo",
+  plugins: ["unused-imports"],
   rules: {
     "quotes": ["error", "double"],
     "semi": ["error", "always"],
-    "no-unused-vars": "warn",
-    "no-console": "warn",
+    "no-unused-vars": "off", // Turn off the base rule as it can report incorrect errors
+    "@typescript-eslint/no-unused-vars": "off", // Turn off the TypeScript rule as it can report incorrect errors
+    "unused-imports/no-unused-imports": "error", // Enable the rule to remove unused imports
+    "unused-imports/no-unused-vars": ["error", { "vars": "all", "varsIgnorePattern": "^_", "args": "after-used", "argsIgnorePattern": "^_" }],
+    // "no-console": "warn",
     "eqeqeq": ["error", "always"],
     "curly": ["error", "all"],
     "brace-style": ["error", "1tbs"],
