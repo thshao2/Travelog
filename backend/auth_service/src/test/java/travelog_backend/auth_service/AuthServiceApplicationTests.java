7a9e65021c4f3f9bac48fa3c5c20ce3ac3acff1c
Brian Nguyen <nguyenbriankhoi@gmail.com>
2024-11-11 19:15:37 -0800

    loaded class context for auth service testing

--- backend/auth_service/src/test/java/travelog_backend/auth_service/AuthServiceApplicationTests.java
+++ backend/auth_service/src/test/java/travelog_backend/auth_service/AuthServiceApplicationTests.java
@@ -3,7 +3,9 @@ package travelog_backend.auth_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import travelog_backend.auth_service.service.AuthServiceTests;
+
+@SpringBootTest(classes = AuthServiceTests.class)
 class AuthServiceApplicationTests {
 
     @Test
