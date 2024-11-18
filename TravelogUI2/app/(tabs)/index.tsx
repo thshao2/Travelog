d50f64be97ae891915b9ac8fe531a9c5fb35c425
thshao <thshao@ucsc.edu>
2024-11-18 02:19:20 -0800

    more aspect ratio modifications

--- TravelogUI2/app/(tabs)/index.tsx
+++ TravelogUI2/app/(tabs)/index.tsx
@@ -3,6 +3,8 @@ import Slider from "react-slick";
 import { Box } from "@mui/material";
 import CategoryList from "../categoryList";
 
+import { AspectRatio } from "@mui/joy";
+
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
@@ -10,7 +12,9 @@ export default function HomeScreen() {
   const images = [
     "https://images8.alphacoders.com/103/1039011.jpg",
     "https://wallpapers.com/images/featured/vegas-4k-9gsywswzyt0y5l6f.jpg",
-    "https://images.alphacoders.com/905/thumb-1920-905423.jpg",
+    "https://wallpapercave.com/wp/wp3948175.jpg",
+    "https://engineering.ucsc.edu/files/2022/09/engineering2building-1024x576.jpg",
+    "https://api.coarchitects.com/wp-content/uploads/2022/08/COJ124_N1283_print.jpg",
   ];
 
   const sliderSettings = {
@@ -28,7 +32,7 @@ export default function HomeScreen() {
       <Box
         sx={{
           width: "100%",
-          height: "65vh", // Carousel takes up 50% of the screen height
+          height: "60vh", // Carousel takes up 50% of the screen height
           // backgroundColor: "white", // Optional: fallback background color
           position: "relative", // Needed for aspect ratio handling
         }}
@@ -36,6 +40,17 @@ export default function HomeScreen() {
 
         <Slider {...sliderSettings}>
           {images.map((image, index) => (
+            // <AspectRatio
+            //   ratio="1200/500" // Use your desired ratio
+            //   key={index}
+            //   sx={{
+            //     maxHeight: "60vh", // Limit height for responsiveness
+            //     width: "100%",
+            //     height: "100%",
+            //     minWidth: "100vh",
+            //     // overflow: "hidden", // Ensures no stretching
+            //   }}
+            // >
             <Box
               key={index}
               component="img"
@@ -44,10 +59,11 @@ export default function HomeScreen() {
               sx={{
                 width: "100%",
                 height: "100%",
-                maxHeight: "65vh",
+                maxHeight: "60vh",
                 objectFit: "cover", // Ensures the full image fits in the container
               }}
             />
+            // </AspectRatio>
           ))}
         </Slider>
       </Box>
