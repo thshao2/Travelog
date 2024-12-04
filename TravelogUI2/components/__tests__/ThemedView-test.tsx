55c28e847733a73ce97dd39e6a7f2f6f497c6cf1
fyyshih <felithecity47@gmail.com>
2024-12-03 23:54:09 -0800

    themedview test + add mollymember to release summary

--- TravelogUI2/components/__tests__/ThemedView-test.tsx
+++ TravelogUI2/components/__tests__/ThemedView-test.tsx
@@ -1,8 +1,41 @@
+import React from "react";
+import { render } from "@testing-library/react-native";
 
-describe("ThemedView Component", () => {
+import { ThemedView } from "../ThemedView";
+import { useThemeColor } from "@/hooks/useThemeColor";
 
-  //   it('test', () => {
-  //      
-  //   });
+// mock useThemeColor hook
+jest.mock("@/hooks/useThemeColor", () => ({
+  useThemeColor: jest.fn(),
+}));
+
+describe("ThemedView", () => {
+  test("renders w default background color", () => {
+    // mock return default bkg color
+    (useThemeColor as jest.Mock).mockReturnValue("#FFFFFF");
+
+    const { getByTestId } = render(<ThemedView testID="themed-view" style={{ height: 100, width: 100 }} />);
+
+    const view = getByTestId("themed-view");
+    expect(view.props.style).toContainEqual({ backgroundColor: "#FFFFFF" });
+  });
+
+  test("renders w lightColor", () => {
+    (useThemeColor as jest.Mock).mockReturnValue("#F0F0F0");
+
+    const { getByTestId } = render(<ThemedView testID="themed-view" lightColor="#F0F0F0" style={{ height: 100, width: 100 }}/>);
+
+    const view = getByTestId("themed-view");
+    expect(view.props.style).toContainEqual({ backgroundColor: "#F0F0F0" });
+  });
+
+  test("renders w darkColor", () => {
+    (useThemeColor as jest.Mock).mockReturnValue("#333333");
+
+    const { getByTestId } = render(<ThemedView testID="themed-view" darkColor="#333333" style={{ height: 100, width: 100 }}/>);
+
+    const view = getByTestId("themed-view");
+    expect(view.props.style).toContainEqual({ backgroundColor: "#333333" });
+  });
 
 });
